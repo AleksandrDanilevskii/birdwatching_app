28d461e96d31d820056c3df287754e95457f52a1
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-02-12 20:11:57 +0300

    Add birds list and bird detail

--- birdwatching/birds/views.py
+++ birdwatching/birds/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from birds.models import Bird
 
 
 def main_page(request):
     return render(request, 'birds/index.html')
+
+
+class BirdsListView(ListView):
+    model = Bird
+
+
+class BirdsDetailView(DetailView):
+    model = Bird
--- birdwatching/birdwatching/urls.py
+++ birdwatching/birdwatching/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from birds.views import main_page
+from birds.views import main_page, BirdsListView, BirdsDetailView
 
 urlpatterns = [
     path('', main_page),
+    path('birds/', BirdsListView.as_view(), name='birds'),
+    path('bird/<int:pk>', BirdsDetailView.as_view(), name='bird'),
     path('admin/', admin.site.urls),
 ]
