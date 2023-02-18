972d328a5698e4782feb4799afa5bb08a618615e
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-02-18 19:13:39 +0300

    Update default date values

--- birdwatching/birds/models.py
+++ birdwatching/birds/models.py
@@ -1,9 +1,8 @@
-import datetime
-
 # from django.contrib.gis.db.models import PointField
 # from django.contrib.gis.geos import Point
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.timezone import now
 
 User = get_user_model()
 
@@ -30,7 +29,7 @@ class Bird(models.Model):
     name = models.CharField(verbose_name='name', max_length=64, unique=True)
     name_latin = models.CharField(verbose_name='name latin', max_length=64, unique=True)
     description = models.TextField(verbose_name='description', max_length=2000, null=True, blank=True)
-    created_at = models.DateTimeField(verbose_name='created at', default=datetime.datetime.now())
+    created_at = models.DateTimeField(verbose_name='created at', default=now)
     updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
 
@@ -54,9 +53,9 @@ class Watch(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     country = models.ForeignKey(Country, on_delete=models.PROTECT)
-    watched_at = models.DateTimeField(verbose_name='watched at', default=datetime.datetime.now())
-    created_at = models.DateTimeField(verbose_name='created at', default=datetime.datetime.now())
-    updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True)
+    watched_at = models.DateTimeField(verbose_name='watched at', default=now)
+    created_at = models.DateTimeField(verbose_name='created at', default=now)
+    updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True, default=now)
     bird = models.ForeignKey(Bird, on_delete=models.PROTECT)
 
     def __str__(self):
