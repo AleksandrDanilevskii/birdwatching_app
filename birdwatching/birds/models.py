8e06663904c5dd88f61ede73367f940f5b1018bc
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-02-19 11:48:25 +0300

    Change model

--- birdwatching/birds/models.py
+++ birdwatching/birds/models.py
@@ -46,14 +46,14 @@ class Country(models.Model):
 
 
 class Watch(models.Model):
-    # objects = models.Manager()
+    objects = models.Manager()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     is_private = models.BooleanField(verbose_name='is private', default=False)
     description = models.TextField(verbose_name='description', max_length=500, null=True, blank=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.00000)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.00000)
     country = models.ForeignKey(Country, on_delete=models.PROTECT)
-    watched_at = models.DateTimeField(verbose_name='watched at', default=now)
+    watched_at = models.DateField(verbose_name='watched at', default=now)
     created_at = models.DateTimeField(verbose_name='created at', default=now)
     updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True, default=now)
     bird = models.ForeignKey(Bird, on_delete=models.PROTECT)
