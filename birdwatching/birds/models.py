c82bea3a60b309192e9cfa38929bb5796be1e38a
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-02-19 12:26:40 +0300

    Add Base class for created_at, updated_ad and trigger in save()

--- birdwatching/birds/models.py
+++ birdwatching/birds/models.py
@@ -1,5 +1,7 @@
 # from django.contrib.gis.db.models import PointField
 # from django.contrib.gis.geos import Point
+import datetime
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.timezone import now
@@ -7,7 +9,19 @@ from django.utils.timezone import now
 User = get_user_model()
 
 
-class Infraclass(models.Model):
+class Base(models.Model):
+    created_at = models.DateTimeField(verbose_name='created at', default=now)
+    updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.datetime.now()
+        super().save(*args, **kwargs)
+
+
+class Infraclass(Base):
     name = models.CharField(verbose_name='name', max_length=64, unique=True)
     description = models.TextField(verbose_name='description', max_length=2000, null=True, blank=True)
 
@@ -15,7 +29,7 @@ class Infraclass(models.Model):
         return f'{self.name}'
 
 
-class Order(models.Model):
+class Order(Base):
     name = models.CharField(verbose_name='name', max_length=64, unique=True)
     description = models.TextField(verbose_name='description', max_length=2000, null=True, blank=True)
     infaclass = models.ForeignKey(Infraclass, on_delete=models.PROTECT)
@@ -24,28 +38,25 @@ class Order(models.Model):
         return f'{self.name} ({self.infaclass.name})'
 
 
-class Bird(models.Model):
+class Bird(Base):
     # objects = models.Manager()
     name = models.CharField(verbose_name='name', max_length=64, unique=True)
     name_latin = models.CharField(verbose_name='name latin', max_length=64, unique=True)
     description = models.TextField(verbose_name='description', max_length=2000, null=True, blank=True)
-    created_at = models.DateTimeField(verbose_name='created at', default=now)
-    updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
 
     def __str__(self):
         return f'{self.name}'
 
 
-# Meta =
-class Country(models.Model):
+class Country(Base):
     name = models.CharField(verbose_name='name', max_length=64, unique=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
-class Watch(models.Model):
+class Watch(Base):
     objects = models.Manager()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     is_private = models.BooleanField(verbose_name='is private', default=False)
@@ -54,8 +65,6 @@ class Watch(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.00000)
     country = models.ForeignKey(Country, on_delete=models.PROTECT)
     watched_at = models.DateField(verbose_name='watched at', default=now)
-    created_at = models.DateTimeField(verbose_name='created at', default=now)
-    updated_at = models.DateTimeField(verbose_name='updated at', null=True, blank=True, default=now)
     bird = models.ForeignKey(Bird, on_delete=models.PROTECT)
 
     def __str__(self):
